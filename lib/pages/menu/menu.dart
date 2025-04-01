87d3152bd9582ef0394832999f5887d297d8f7e4
VICTOR BARBOSA <victor.barbosa@enkode.tech>
2025-04-01 13:00:34 -0300

    Backup Macbook

--- lib/pages/menu/menu.dart
+++ lib/pages/menu/menu.dart
@@ -17,57 +17,92 @@ class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(children: [
-          Container(
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Center(
-                  child: Text(
-                    "Quick Reflex",
-                    style: TextStyle(fontSize: 30, color: Colors.white),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+        child: SafeArea(
+          child: Stack(children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.black,
+                      Color.fromARGB(255, 83, 83, 83),
+                    ],
+                    begin: FractionalOffset(0.0, 0.0),
+                    end: FractionalOffset(0.0, 1.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Center(
+                    child: Text(
+                      "Quick Reflex",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 45,
+                          color: Colors.white),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                MaterialButton(
-                  onPressed: () => _quickController.setClickPlay(true),
-                  color: Colors.amber,
-                  child: const Text("Jogar"),
-                ),
-                MaterialButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) {
-                        return ShowRecords(
-                          quickReflexController: _quickController,
-                        );
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  SizedBox(
+                    width: 280,
+                    child: MaterialButton(
+                      onPressed: () => _quickController.setClickPlay(true),
+                      color: const Color.fromARGB(255, 255, 218, 106),
+                      child: const Text("Jogar",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45,
+                              color: Color.fromARGB(255, 141, 119, 53))),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  SizedBox(
+                    width: 280,
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) {
+                            return ShowRecords(
+                              quickReflexController: _quickController,
+                            );
+                          },
+                        ));
                       },
-                    ));
-                  },
-                  color: Colors.amber,
-                  child: const Text("Recordes"),
-                )
-              ],
+                      color: const Color.fromARGB(255, 255, 218, 106),
+                      child: const Text("Recordes",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 45,
+                              color: Color.fromARGB(255, 141, 119, 53))),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Observer(
-            builder: (context) {
-              return _quickController.isClickPlay
-                  ? Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 48, horizontal: 24),
-                      child: PrePlay(
-                        quickReflexController: _quickController,
-                      ),
-                    )
-                  : const SizedBox();
-            },
-          )
-        ]),
+            Observer(
+              builder: (context) {
+                return _quickController.isClickPlay
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 48, horizontal: 24),
+                        child: PrePlay(
+                          quickReflexController: _quickController,
+                        ),
+                      )
+                    : const SizedBox();
+              },
+            )
+          ]),
+        ),
       ),
     );
   }
